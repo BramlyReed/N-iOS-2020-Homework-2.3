51bb9ee0a23c6b16589bcc9b96a03b4cd312b604
BramlyReed <ya.andreev99@ya.ru>
2020-11-17 03:41:41 -0800

    Initial Commit

---
+++ N-iOS-2020-Homework-2.3Tests/N_iOS_2020_Homework_2_3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  N_iOS_2020_Homework_2_3Tests.swift
+//  N-iOS-2020-Homework-2.3Tests
+//
+//  Created by Stas on 17.11.2020.
+//
+
+import XCTest
+@testable import N_iOS_2020_Homework_2_3
+
+class N_iOS_2020_Homework_2_3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
